fb4d5033be5de5834b383905b8e59b4eca61a308
Ose <Agbaleose@gmail.com>
2025-01-02 05:11:59 +0100

    Day1 challenge done

---
+++ Challenges/Day_1/Myday1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash     
+#The first line is a shebang. This tells the shell that it is a bash shell.
+
+#echo command
+echo "Ose is new to scripting and is not gonna give up till scripting is a new norm".
+
+#Variables
+variable1="Hello"
+variable2="Terminator!"
+
+#using variables
+greeting="$variable1 $variable2"
+echo "$greeting you have to protect whats yours!."
+
+#using built-in variables
+echo "The name of the shell that is being used is" -$SHELL
+echo "The user ID of the current user is" -$UID
+echo "The name of the current logged in user is" -$USER
+echo "The current user's home directory is -$HOME
+
+
+
